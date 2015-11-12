0b25c472c621379b81f124cc7cdbbd2224a35b35
Adebayo <mradeybee@gmail.com>
2015-11-12 19:14:30 +0100

    Booking Model  tests written

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,4 @@
 class Booking < ActiveRecord::Base
-	include AddCode
 	before_create :add_code
 	belongs_to :flight
   has_many :passengers
@@ -8,5 +7,5 @@ class Booking < ActiveRecord::Base
   def add_code
      raw = [('A'..'Z'),(0..9)].map { |i| i.to_a }.flatten
      self.code = (0...20).map { raw[rand(raw.length)] }.join
-   end
+  end
 end
