b2630304eb04f5682df074626533471694d3a5f9
Adebayo <mradeybee@gmail.com>
2015-11-12 19:43:24 +0100

    proposed email fix for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   # Do not eager load code on boot.
   config.eager_load = false
-
+  
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
