114fed63eedec4617a7b756927409dd4d7379a0c
Adebayo <mradeybee@gmail.com>
2015-11-14 06:22:11 +0100

    third debuging

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -6,13 +6,15 @@ class Flight < ActiveRecord::Base
   accepts_nested_attributes_for :passengers
   accepts_nested_attributes_for :bookings
 
-  def self.search(origin, destination)
-    # departure_date = "#{departure_date}%"
-    Flight.where("origin_id = ? AND destination_id = ?", origin, destination)
+  def self.search(origin, destination, departure_date)
+    date = Time.zone.parse(departure_date)
+    range = date + 1.month if date
+    Flight.where(destination: destination, origin: origin, departure_date: date..range)   
   end
 
   def sort_by_date(departure_date)
   	Flight.where("strftime('%Y-%m-%d', departure_date)=?", departure_date)
   end
 end
- 
+   # departure_date = "#{departure_date}%"
+      # Flight.where("origin_id = ? AND destination_id = ? AND departure_date LIKE ?", origin, destination, departure_date)
