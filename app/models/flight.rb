b305b16162539272f2b2a5b6ee9545643c708256
Adebayo <mradeybee@gmail.com>
2015-11-14 06:36:13 +0100

    final debug, merging to master

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -16,5 +16,3 @@ class Flight < ActiveRecord::Base
   	Flight.where("strftime('%Y-%m-%d', departure_date)=?", departure_date)
   end
 end
-   # departure_date = "#{departure_date}%"
-      # Flight.where("origin_id = ? AND destination_id = ? AND departure_date LIKE ?", origin, destination, departure_date)
