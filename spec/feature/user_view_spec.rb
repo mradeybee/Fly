2718f2b37629beccd6cbeace63e58cdfdc3411f4
Adebayo <mradeybee@gmail.com>
2015-11-25 18:30:44 +0100

    failing test refactored

--- spec/feature/user_view_spec.rb
+++ spec/feature/user_view_spec.rb
@@ -85,9 +85,8 @@ RSpec.feature "UserView"  do
   describe "Book Flight", js: true do
     it "Books a fligth" do
       visit "/flights"
+      sleep 2
       first(:button, "Select").click
-      click_on "Log In"
-      click_on 'Facebook'
       sleep 2
       click_link ('Add Passenger')
       fill_in('name', with: 'Adebayo')
@@ -95,5 +94,6 @@ RSpec.feature "UserView"  do
       click_on 'Book Now'
       expect(page).to have_content "successful"
    end
+
   end
 end
